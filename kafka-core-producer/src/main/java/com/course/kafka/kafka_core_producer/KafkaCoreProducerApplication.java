bb84ed3c348f74c33875c791edc40695b82b92a8
My Name <me@gm.com>
2025-02-22 20:11:38 +0100

    8.34-hello-kafka-Java-Spring-Code : create a service producing message in kafaka topic + inject the service in springbootapplication to produce message

--- kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/KafkaCoreProducerApplication.java
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/KafkaCoreProducerApplication.java
@@ -1,14 +1,31 @@
 package com.course.kafka.kafka_core_producer;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.course.kafka.kafka_core_producer.producer.HelloKafkaProducer;
+
 
 @SpringBootApplication
-public class KafkaCoreProducerApplication {
+public class KafkaCoreProducerApplication implements CommandLineRunner {
+
+	private HelloKafkaProducer helloKafkaProducer;
+
+	public KafkaCoreProducerApplication(HelloKafkaProducer helloKafkaProducer) {
+		this.helloKafkaProducer = helloKafkaProducer;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaCoreProducerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		helloKafkaProducer.sendHello("MedGaz "+ ThreadLocalRandom.current().nextInt(1000));
 	}
 
 }
