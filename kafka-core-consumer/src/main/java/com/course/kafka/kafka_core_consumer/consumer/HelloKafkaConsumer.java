480a1f10a28c874178721e0f67c1a4656046cf1d
My Name <me@gm.com>
2025-02-22 20:12:32 +0100

    8.34-hello-kafka-Java-Spring-Code : implement a  KafKaListener service to consume the produced message

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/HelloKafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.course.kafka.kafka_core_consumer.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloKafkaConsumer {
+
+    @KafkaListener(topics = "t-hello")
+    public void consumeHello(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+}
