199274fe1ad3fabef83dd6cdd22a00c09dafe23e
My Name <me@gm.com>
2025-02-23 16:58:25 +0100

    8.39-fixing-consumer : disable previous producer recording message on t-fixedrate

--- kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/producer/FixedRateAIProducer.java
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/producer/FixedRateAIProducer.java
@@ -6,7 +6,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class FixedRateAIProducer {
     
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(FixedRateAIProducer.class);
