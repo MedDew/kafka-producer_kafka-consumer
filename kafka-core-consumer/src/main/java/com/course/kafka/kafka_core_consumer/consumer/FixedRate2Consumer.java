69940215ff1ff2281dff08c4a367270b39aa9cee
My Name <me@gm.com>
2025-02-23 17:04:10 +0100

    8.39-fixing-consumer : implement a consumer consumming message from t-fixedrate-2 topic

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/FixedRate2Consumer.java
@@ -0,0 +1,18 @@
+package com.course.kafka.kafka_core_consumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRate2Consumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FixedRate2Consumer.class);
+
+    @KafkaListener(topics = "t-fixedrate-2")
+    public void consumeMessage(String message) {
+        LOG.info("t-fixedrate-2 consumed message with auto-offset-reset set to earliest: " + message);
+    }
+
+}
