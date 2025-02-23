b963c2aeaf03a42255700b5d42ec5b10a67cfae4
My Name <me@gm.com>
2025-02-23 17:01:27 +0100

    8.39-fixing-consumer : create a producer incrementing a int value to record it into t-fixed-rate-2 topic + assign the auto-offset-reset kafka configuration key to the earliest value so the message consummed are done from the beginning not from the moment the consumer has started

---
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/producer/FixedRate2.java
@@ -0,0 +1,28 @@
+package com.course.kafka.kafka_core_producer.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRate2 {
+
+    private static final Logger log = LoggerFactory.getLogger(FixedRate2.class);
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private int i = 0;
+
+    public FixedRate2(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void sendMessage() {
+        i++;
+        log.info("Sending message to t-fixedrate-2 topic: {}", "I ::> " + i);
+        kafkaTemplate.send("t-fixedrate-2",  "Fixed rate 2 => " + i);
+    }
+
+
+}
