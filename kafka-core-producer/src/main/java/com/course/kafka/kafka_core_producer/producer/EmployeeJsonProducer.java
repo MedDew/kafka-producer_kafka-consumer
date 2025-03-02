d06fe02b8c981bf91924b8fc8e8d54dd17be5959
My Name <me@gm.com>
2025-03-02 16:14:33 +0100

    9.43-producing-json-message : implement a kafka producer to send message into t-employee topic with the use of KafkaTemplate

---
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/producer/EmployeeJsonProducer.java
@@ -0,0 +1,30 @@
+package com.course.kafka.kafka_core_producer.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.course.kafka.kafka_core_producer.entity.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class EmployeeJsonProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public void sendMessage(Employee employee) {
+        // convert (serialize) Employee object to JSON and publist to t-employee topic
+
+        try {
+            String json = objectMapper.writeValueAsString(employee);
+            kafkaTemplate.send("t-employee", json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+}
