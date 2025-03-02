412c9ed684893761105d07fe2d6541d886a4d546
My Name <me@gm.com>
2025-03-02 17:38:20 +0100

    9.45-consume-json-message : create a jackson mapper bean to deserialize the message consummed from t-employee- topic

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/config/JsonConfig.java
@@ -0,0 +1,20 @@
+package com.course.kafka.kafka_core_consumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+}
