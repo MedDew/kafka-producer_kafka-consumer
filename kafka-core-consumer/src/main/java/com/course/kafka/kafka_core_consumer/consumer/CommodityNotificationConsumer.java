1a24b0da4db717eed1841fd85311484598c1fd1d
My Name <me@gm.com>
2025-04-13 18:58:10 +0200

    9.47-consuming-with-consumer-group : implement 2 different kafka consumer using group id overriding the application group intended to parallelized kafka message consumption

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/CommodityNotificationConsumer.java
@@ -0,0 +1,30 @@
+package com.course.kafka.kafka_core_consumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.course.kafka.kafka_core_consumer.entity.Commodity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class CommodityNotificationConsumer {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static final Logger LOG = LoggerFactory.getLogger(CommodityNotificationConsumer.class);
+
+    @KafkaListener(topics = "t-commodity", groupId = "consumer-group-notification")
+    private void listen(String message) {
+        try {
+            Commodity commodity = objectMapper.readValue(message, Commodity.class);
+            LOG.info("Notification consumer: {}", commodity);
+        } catch (Exception e) {
+            LOG.error("Error processing message: {}", message, e);
+        }
+    }
+
+}
