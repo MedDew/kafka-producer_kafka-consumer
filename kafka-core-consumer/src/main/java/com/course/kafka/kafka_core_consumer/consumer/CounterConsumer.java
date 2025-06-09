0620b59e0ec1299a045405e432f2357884f64461
My Name <me@gm.com>
2025-06-09 19:15:37 +0200

    9.49-configure-kafka-spring : disable t-counter topic listener

--- kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/CounterConsumer.java
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/CounterConsumer.java
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CounterConsumer {
 
-    @KafkaListener(topics = "t-counter", groupId = "counter-group-fast")
+    // @KafkaListener(topics = "t-counter", groupId = "counter-group-fast")
     public void consumeFast(String message) {
         System.out.println("Fast : " + message);
     }
 
-    @KafkaListener(topics = "t-counter", groupId = "counter-group-slow")
+    // @KafkaListener(topics = "t-counter", groupId = "counter-group-slow")
     public void consumeSlow(String message) throws InterruptedException {
         TimeUnit.SECONDS.sleep(3); // Simulating slow processing
         System.out.println("Slow : " + message);
