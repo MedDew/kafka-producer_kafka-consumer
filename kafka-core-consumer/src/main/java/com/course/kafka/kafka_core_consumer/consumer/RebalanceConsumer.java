5919551372833c4aa801da20d84674ca0014b292
My Name <me@gm.com>
2025-09-30 17:38:32 +0200

    9.52-rebalancing : deactivate kafka listner with RoundRobinAssignor egaer balancing strategy to switch back to the default balancing stratey and avoid InconsistentGroupProtocolException at application startup since t-rebalance-alpha has 5 partitions and t-rebalance-beta has 2 partitions

--- kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/RebalanceConsumer.java
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/RebalanceConsumer.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class RebalanceConsumer {
 
     @KafkaListener(topics = { "t-rebalance-alpha", "t-rebalance-beta" }, groupId = "group-rebalance", concurrency = "3")
