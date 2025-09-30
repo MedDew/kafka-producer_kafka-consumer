78641d330cb062054ab0d4e2d962c9641b81be71
My Name <me@gm.com>
2025-09-30 17:32:08 +0200

    9.52-rebalancing : create a kafka listener to depict eager balancing using RoundRobinAssignor eager rebalancing strategy

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/RebalanceConsumer.java
@@ -0,0 +1,15 @@
+package com.course.kafka.kafka_core_consumer.consumer;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RebalanceConsumer {
+
+    @KafkaListener(topics = { "t-rebalance-alpha", "t-rebalance-beta" }, groupId = "group-rebalance", concurrency = "3")
+    public void consume(String message) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(5);
+    }
+}
