ce6cab46a494fa2c4ecfcae7b724edc5dd423fe2
My Name <me@gm.com>
2025-10-01 13:10:53 +0200

    9.54-idempotency : implement a producer to record PurchaseRequest into t-purchase-request topic using a unique message key with purchaseRequest.getRequestNumber()

---
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/producer/PurchaseRequestProducer.java
@@ -0,0 +1,34 @@
+package com.course.kafka.kafka_core_producer.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.course.kafka.kafka_core_producer.entity.PurchaseRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oracle.graal.compiler.enterprise.p;
+
+@Service
+public class PurchaseRequestProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private ObjectMapper objectMapper;
+
+    public PurchaseRequestProducer(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendPurchaseRequest(PurchaseRequest purchaseRequest) {
+        // Serialize the object to JSON
+        String purchaseRequestJson;
+        try {
+            purchaseRequestJson = objectMapper.writeValueAsString(purchaseRequest);
+            kafkaTemplate.send("t-purchase-request", purchaseRequest.getRequestNumber(), purchaseRequestJson);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
