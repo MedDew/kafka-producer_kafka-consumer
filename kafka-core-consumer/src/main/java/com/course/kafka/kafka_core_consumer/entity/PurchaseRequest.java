d3699b65dfd0f002d5fee6bbb8ad5d4b99ee3bf3
My Name <me@gm.com>
2025-10-01 13:59:59 +0200

    9.54-idempotency : create a PurchaseRequest to read data from t-purchase-request topic

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/entity/PurchaseRequest.java
@@ -0,0 +1,63 @@
+package com.course.kafka.kafka_core_consumer.entity;
+
+import java.util.UUID;
+
+public class PurchaseRequest {
+
+    private UUID requestId;
+
+    private String requestNumber;
+
+    private int amount;
+
+    private String currency;
+
+    public PurchaseRequest() {
+    }
+
+    public PurchaseRequest(UUID requestId, String requestNumber, int amount, String currency) {
+        this.requestId = requestId;
+        this.requestNumber = requestNumber;
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public UUID getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(UUID requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getRequestNumber() {
+        return requestNumber;
+    }
+
+    public void setRequestNumber(String requestNumber) {
+        this.requestNumber = requestNumber;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "PurchaseRequest [requestId=" + requestId + ", requestNumber=" + requestNumber + ", amount=" + amount
+                + ", currency=" + currency + "]";
+    }
+
+}
