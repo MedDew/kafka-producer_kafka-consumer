1b68de27d42b752fd3a8dc32d7b30a08b5b7475b
My Name <me@gm.com>
2025-10-01 14:22:26 +0200

    9.54-idempotency : create a consumer checking the existence of the message processed and adding to the cache the unprocessed messages

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/consumer/PurchaseRequestConsumer.java
@@ -0,0 +1,48 @@
+package com.course.kafka.kafka_core_consumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.course.kafka.kafka_core_consumer.entity.PurchaseRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.benmanes.caffeine.cache.Cache;
+
+@Service
+public class PurchaseRequestConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PurchaseRequestConsumer.class);
+
+    private ObjectMapper objectMapper;
+
+    private Cache<String, Boolean> cachePurchaserequest;
+
+    public PurchaseRequestConsumer(ObjectMapper objectMapper, Cache<String, Boolean> cachePurchaserequest) {
+        this.objectMapper = objectMapper;
+        this.cachePurchaserequest = cachePurchaserequest;
+    }
+
+    private boolean isExistsInCache(String requestNumber) {
+        Boolean exists = cachePurchaserequest.getIfPresent(requestNumber);
+        return exists != null && exists;
+    }
+
+    @KafkaListener(topics = "t-purchase-request")
+    public void listen(String json) {
+        try {
+
+            PurchaseRequest purchaseRequest = objectMapper.readValue(json, PurchaseRequest.class);
+            if (isExistsInCache(purchaseRequest.getRequestNumber())) {
+                LOG.warn("Purchase request already exists in cache : {}", purchaseRequest.getRequestNumber());
+                return;
+            }
+
+            LOG.info("Processing purchase request : {}", purchaseRequest.getRequestNumber());
+            cachePurchaserequest.put(purchaseRequest.getRequestNumber(), true);
+
+        } catch (Exception e) {
+            LOG.error("Error processing purchase request", e);
+        }
+    }
+}
