c8dd63991a9720fd08cc4146f69768234bcfad49
My Name <me@gm.com>
2025-10-01 13:57:59 +0200

    9.54-idempotency : caffeine cache managment dependency + create the caffeine bean to handle cache

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/config/CacheConfig.java
@@ -0,0 +1,22 @@
+package com.course.kafka.kafka_core_consumer.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Cache<String, Boolean> cachePurchaserequest() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .maximumSize(1000)
+                .build();
+    }
+
+}
