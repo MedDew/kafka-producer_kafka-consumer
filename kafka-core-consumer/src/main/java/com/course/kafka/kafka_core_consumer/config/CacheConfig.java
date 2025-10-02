99eceecd847a53831074f0479288b96b5b62c99a
My Name <me@gm.com>
2025-10-02 13:20:34 +0200

    9.55-idempotency-alternative-approach : create a cachePaymentRequest bean to save in cache t-payment-request topic message consumed data adding name to each beans since they return the same type

--- kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/config/CacheConfig.java
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/config/CacheConfig.java
@@ -11,8 +11,16 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 @Configuration
 public class CacheConfig {
 
-    @Bean
+    @Bean(name = "cachePurchaserequest")
     public Cache<String, Boolean> cachePurchaserequest() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(5, TimeUnit.MINUTES)
+                .maximumSize(1000)
+                .build();
+    }
+
+    @Bean(name = "cachePaymentRequest")
+    public Cache<String, Boolean> cachePaymentRequest() {
         return Caffeine.newBuilder()
                 .expireAfterWrite(5, TimeUnit.MINUTES)
                 .maximumSize(1000)
