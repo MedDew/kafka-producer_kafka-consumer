7f37839e7bad03f5e704b547b5cbee4a872e6fae
My Name <me@gm.com>
2025-10-03 15:21:37 +0200

    10.57-kafkalistener-error-handler : create records to be saved into t-food-order topic

--- kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/KafkaCoreProducerApplication.java
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/KafkaCoreProducerApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.course.kafka.kafka_core_producer.entity.FoodOrder;
 import com.course.kafka.kafka_core_producer.entity.PaymentRequest;
+import com.course.kafka.kafka_core_producer.producer.FoodOrderProducer;
 import com.course.kafka.kafka_core_producer.producer.PaymentRequestProducer;
 
 @SpringBootApplication
@@ -16,10 +18,10 @@ public class KafkaCoreProducerApplication implements CommandLineRunner {
 
 	private static final Logger LOG = LoggerFactory.getLogger(KafkaCoreProducerApplication.class);
 
-	private PaymentRequestProducer paymentRequestProducer;
+	private FoodOrderProducer foodOrderProducer;
 
-	public KafkaCoreProducerApplication(PaymentRequestProducer paymentRequestProducer) {
-		this.paymentRequestProducer = paymentRequestProducer;
+	public KafkaCoreProducerApplication(FoodOrderProducer foodOrderProducer) {
+		this.foodOrderProducer = foodOrderProducer;
 	}
 
 	public static void main(String[] args) {
@@ -28,30 +30,13 @@ public class KafkaCoreProducerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		PaymentRequest paymentRequest1 = new PaymentRequest(100, "USD", "987-654", "Payment for invoice 789",
-				LocalDate.now().plusDays(2));
-		PaymentRequest paymentRequest2 = new PaymentRequest(200, "CAN", "123-456", "Payment for invoice 123",
-				LocalDate.now());
-		PaymentRequest paymentRequest3 = new PaymentRequest(300, "EUR", "789-012", "Payment for invoice 456",
-				LocalDate.now().plusDays(5));
-		PaymentRequest paymentRequest4 = new PaymentRequest(400, "EUR", "789-012", "Payment for invoice 456",
-				LocalDate.now().plusDays(10));
-		PaymentRequest paymentRequest5 = new PaymentRequest(500, "EUR", "789-012", "Payment for invoice 456",
-				LocalDate.now().plusDays(15));
-		PaymentRequest paymentRequest6 = new PaymentRequest(600, "EUR", "789-012", "Payment for invoice 456",
-				LocalDate.now().plusDays(20));
+		FoodOrder chickenOrder = new FoodOrder(3, "Chicken");
+		FoodOrder fishOrder = new FoodOrder(10, "Fish");
+		FoodOrder pizzaOrder = new FoodOrder(5, "Pizza");
 
-		paymentRequestProducer.sendPaymentRequest(paymentRequest1);
-		paymentRequestProducer.sendPaymentRequest(paymentRequest2);
-		paymentRequestProducer.sendPaymentRequest(paymentRequest3);
-		paymentRequestProducer.sendPaymentRequest(paymentRequest4);
-		paymentRequestProducer.sendPaymentRequest(paymentRequest5);
-		paymentRequestProducer.sendPaymentRequest(paymentRequest6);
-
-		// Simulating a duplicate message recorded into t-payment-request topic
-		paymentRequestProducer.sendPaymentRequest(paymentRequest1);
-		paymentRequestProducer.sendPaymentRequest(paymentRequest2);
-
+		foodOrderProducer.sendFoodOrder(chickenOrder);
+		foodOrderProducer.sendFoodOrder(fishOrder);
+		foodOrderProducer.sendFoodOrder(pizzaOrder);
 	}
 
 	/*
