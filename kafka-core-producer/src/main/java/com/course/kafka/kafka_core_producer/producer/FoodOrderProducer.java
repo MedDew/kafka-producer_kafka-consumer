48479ef1ae3091130d7936985a0e7f7caf4100e7
My Name <me@gm.com>
2025-10-03 15:17:21 +0200

    10.57-kafkalistener-error-handler : create FoodOrder producer to save FoodOrder message in t-food-order

---
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/producer/FoodOrderProducer.java
@@ -0,0 +1,30 @@
+package com.course.kafka.kafka_core_producer.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.course.kafka.kafka_core_producer.entity.FoodOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class FoodOrderProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private ObjectMapper objectMapper;
+
+    public FoodOrderProducer(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendFoodOrder(FoodOrder foodOrder) {
+        try {
+            String foodOrderJson = objectMapper.writeValueAsString(foodOrder);
+            kafkaTemplate.send("t-food-order", foodOrderJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
