854a5a83b0124db9596ca5c2a0684e329e7a1413
My Name <me@gm.com>
2025-10-03 16:53:04 +0200

    10.58-common-error-handler :   create an entity to consume metadat form messages saved into t-simple-number topic

---
+++ kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/entity/SimpleNumber.java
@@ -0,0 +1,26 @@
+package com.course.kafka.kafka_core_consumer.entity;
+
+public class SimpleNumber {
+    private int number;
+
+    public SimpleNumber() {
+    }
+
+    public SimpleNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleNumber [number=" + number + "]";
+    }
+
+}
