2a56c4ce80b194bcefd9d1fbc7cab3152efcdc94
My Name <me@gm.com>
2025-10-04 14:21:24 +0200

    10.59-consumer-with-retry : implement a producer saving Image entity as a message into t-image topic

---
+++ kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/producer/ImageProducer.java
@@ -0,0 +1,29 @@
+package com.course.kafka.kafka_core_producer.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.course.kafka.kafka_core_producer.entity.Image;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class ImageProducer {
+
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    private ObjectMapper objectMapper;
+
+    public ImageProducer(KafkaTemplate<String, Object> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void send(Image image, int partition) {
+        try {
+            String imageJson = objectMapper.writeValueAsString(image);
+            kafkaTemplate.send("t-image", partition, image.getType(), imageJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
